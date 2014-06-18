c8c3c84e46c40da109e9243daed6d28cec913d9d
bloodmage <zhouyisu@gmail.com>
2014-06-18 17:53:18 +0800

    valid for labeling

--- hengelossvalid.py
+++ hengelossvalid.py
@@ -37,3 +37,23 @@ def binaryloss(layerout, label, thresholdlayer, thresholdlabel):
     F = 2*precision*recall / (precision + recall)
 
     return obinary, tpos,fpos,tneg,fneg, F
+
+def binaryloss_label(layerout, label):
+    maxaxes = T.max(layerout, axis=1, keepdims=True)
+    obinary = T.switch(T.ge(layerout, maxaxes), 1.0, 0.0)
+    lbinary = T.switch(T.gt(label, thresholdlabel), 1.0, 0.0)
+
+    positives = T.sum(lbinary, axis=(0, 2, 3))
+    negatives = T.sum(1-lbinary, axis=(0, 2, 3))
+
+    tpos = T.sum(lbinary * obinary, axis=(0, 2, 3))
+    fneg = T.sum(lbinary * (1-obinary), axis=(0, 2, 3))
+    tneg = T.sum((1-lbinary) * (1-obinary), axis=(0, 2, 3))
+    fpos = T.sum((1-lbinary) * obinary, axis=(0, 2, 3))
+
+    precision = tpos / (tpos + fpos)
+    recall = tpos / (tpos + fneg)
+    F = 2*precision*recall / (precision + recall)
+
+    return obinary, tpos,fpos,tneg,fneg, F
+
