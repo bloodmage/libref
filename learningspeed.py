52c14af609da000e93c88b2676f57371bc1e44fc
bloodmage <zhouyisu@gmail.com>
2014-08-11 14:51:24 +0800

    regulated learning

---
+++ learningspeed.py
@@ -0,0 +1,45 @@
+import layerbase
+import theano
+import theano.tensor as T
+import numpy as np
+class learningspeed:
+    def __init__(self,layertarget = 1.0, layerstr = 0.1, baserate = 1.0, basedynamic = 1e-3):
+        #self.cellrate,self.cellstr = cellrate, cellstr
+        self.layertarget,self.layerstr = layertarget, layerstr
+        self.baserate = baserate
+        self.basedynamic = basedynamic
+
+    def fixspeed(self,model,momentums):
+        paramlayers = model.paramslayers()
+        coeff = []
+        outs = []
+        pid = 0
+        for paramlayer in paramlayers:
+            #For W
+            D = 0
+            if isinstance(paramlayer,(layerbase.ConvLayer,layerbase.ConvMaxoutLayer,layerbase.ConvKeepLayer)):
+                layershape = paramlayer.params[0].get_value().shape
+                fan_in = layershape[1]*layershape[2]*layershape[3]
+                fan_out = np.prod(layershape)
+                D = 1
+            elif isinstance(paramlayer,(layerbase.FullConnectLayer)):
+                layershape = paramlayer.params[0].get_value().shape
+                fan_in = layershape[0]
+                fan_out = np.prod(layershape)
+                D = 1
+            else:
+                coeff.append(1)
+            if D:
+                layerrate = (self.layertarget*fan_out/fan_in) / T.sum(abs(momentums[pid])) * self.layerstr + self.baserate * (1-self.layerstr) * self.basedynamic
+                coeff.append(layerrate)
+                outs.append(momentums[pid]*layerrate)
+                pid+=1
+            #For other
+            for i in paramlayer.params[D:]:
+                outs.append(momentums[pid]*self.baserate)
+                pid+=1
+        return outs,coeff
+
+if __name__=="__main__":
+    pass
+
---
+++ libwhite.py
@@ -0,0 +1,49 @@
+import numpy.fft as fft
+import numpy as np
+import os
+
+whiteconfs = {}
+
+def white(arr4d, cachename):
+    if os.path.exists(cachename+'.npz'):
+        cache = np.load(cachename)
+        subterm = cache['sub']
+        divterm = cache['div']
+        fftterm = cache['fft']
+        whiteconfs[cachename] = [subterm,divterm,fftterm]
+        arr4d = (arr4d - subterm) * divterm
+        farr4d = fft.rfft2(arr4d)
+        farr4d = farr4d * fftterm
+        arr4d = fft.irfft2(farr4d)
+        return arr4d
+
+    subterm = np.mean(arr4d,0,keepdims = True)
+    divterm = 1.0/(arr4d.std(axis=(2,3),keepdims=True)+1e-10)
+    arr4d = (arr4d - subterm) * divterm
+    farr4d = fft.rfft2(arr4d)
+    fftterm = 1.0/(np.sqrt(np.mean(np.abs(farr4d)**2,axis=0))+1e-10)
+    farr4d = farr4d * fftterm
+    arr4d = fft.irfft2(farr4d)
+    np.savez(cachename+'.npz',sub=subterm,div=divterm,fft=fftterm)
+    whiteconfs[cachename] = [subterm,divterm,fftterm]
+    return arr4d
+
+def unwhite(arr4d, cachename):
+    assert cachename in whiteconfs
+    subterm, divterm, fftterm = whiteconfs[cachename]
+    farr4d = fft.rfft2(arr4d)
+    farr4d = farr4d / fftterm
+    arr4d = fft.irfft2(farr4d)
+    arr4d = arr4d / divterm + subterm
+    return arr4d
+
+if __name__=="__main__":
+    import numpy.random as npr
+    d = npr.ranf((10,1,20,20))
+    dw = white(d,'test')
+    d2 = unwhite(dw,'test')
+    os.unlink('test.npz')
+    print d
+    print dw
+    print np.sum(np.abs(d2-d))
+
